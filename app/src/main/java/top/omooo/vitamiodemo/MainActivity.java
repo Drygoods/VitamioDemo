59e70cf8f49f4c965ad864a9e9e6ad3d45a01101
Omooo <869759698@qq.com>
2017-12-06 15:11:02 +0800

    基本可以播放直播流

--- app/src/main/java/top/omooo/vitamiodemo/MainActivity.java
+++ app/src/main/java/top/omooo/vitamiodemo/MainActivity.java
@@ -3,15 +3,27 @@ package top.omooo.vitamiodemo;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import com.dou361.ijkplayer.widget.PlayStateParams;
+import com.dou361.ijkplayer.widget.PlayerView;
+
 
 public class MainActivity extends AppCompatActivity {
 
+    private PlayerView player;
     private String url = "http://117.144.248.49/HDhnws.m3u8?authCode=07110409322147352675&amp;stbId=006001FF0018120000060019F0D49A1&amp;Contentid=6837496099179515295&amp;mos=jbjhhzstsl&amp;livemode=1&amp;channel-id=wasusyt";
     private String url2 = "http://www.wooyun.site/1987.mp4";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+//        setContentView(R.layout.activity_main);
+        setContentView(R.layout.simple_player_view_player);
+        player = new PlayerView(this)
+                .setTitle("什么")
+                .setScaleType(PlayStateParams.fitparent)
+                .hideMenu(true)
+                .forbidTouch(false)
+                .setPlaySource(url)
+                .startPlay();
     }
-
 }
